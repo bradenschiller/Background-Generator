473c758cfeaf85dcd7c70f6a6143aaefa5e0be9f
fitnessmodels <admin@fitnessmodels.com>
2018-10-05 11:08:09 -0500

    changed function to templating strings

--- app.js
+++ app.js
@@ -4,19 +4,11 @@ const input2 = document.getElementById("input2");
 const body = document.querySelector("body");
 
 const changeBackground = () => {
-    body.style.background = 
-    "linear-gradient(to right, " +
-    input1.value +
-    ", " +
-    input2.value +
-    ")"
-
-    content.textContent = body.style.background + ";"
+    body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`
+    content.textContent = `${body.style.background};`
 }
 
-
 input1.addEventListener("change", changeBackground)
-
 input2.addEventListener("change", changeBackground)
 
 
